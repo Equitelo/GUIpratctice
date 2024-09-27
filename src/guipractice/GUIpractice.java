e19b83abac2cfb1ac859a322a6fbe4856e970155
Equitelo <samuel_lara@unitec.edu>
2024-09-26 20:51:02 -0600

    Trabajo con clases y objetos, junto con implementacion de eventos!

--- src/guipractice/GUIpractice.java
+++ src/guipractice/GUIpractice.java
@@ -1,53 +1,11 @@
 package guipractice;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
-
 public class GUIpractice {
 
     public static void main(String[] args) {
-        //JLayeredPane = Swing container that provides a 
-        //               third dimension for positioning components
-        //               ex. depth, Z-index
         
-        JLabel label1 = new JLabel();
-        label1.setOpaque(true);
-        label1.setBackground(Color.RED);
-        label1.setBounds(50, 50, 200, 200);
+        MyFrame myFrame = new MyFrame();
         
-        JLabel label2 = new JLabel();
-        label2.setOpaque(true);
-        label2.setBackground(Color.GREEN);
-        label2.setBounds(100, 100, 200, 200);
-        
-        JLabel label3 = new JLabel();
-        label3.setOpaque(true);
-        label3.setBackground(Color.BLUE);
-        label3.setBounds(150, 150, 200, 200);
-        
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setBounds(0, 0, 500, 500);
-        
-        //higher the number, higher the placement is:
-        
-        layeredPane.add(label1, Integer.valueOf(0));
-        layeredPane.add(label2, Integer.valueOf(2));
-        layeredPane.add(label3, Integer.valueOf(1));
-        
-        JFrame frame = new JFrame("JLayeredPane");
-        frame.add(layeredPane);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setLayout(null);
-        frame.setVisible(true);
     }
     
 }
