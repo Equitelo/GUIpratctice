1418eead3e5f04f9fad409ea501ae050ca573d12
Equitelo <samuel_lara@unitec.edu>
2024-09-27 19:56:09 -0600

    Ahora entiendo la funcionalidad de los JOptionPanes, como colocarle iconos, colocarle mis propias opciones con arreglos y los diferentes tipos que hay.

--- src/guipractice/GUIpractice.java
+++ src/guipractice/GUIpractice.java
@@ -1,10 +1,32 @@
 package guipractice;
 
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
 public class GUIpractice {
 
     public static void main(String[] args) {
         
-        MyFrame myFrame = new MyFrame();
+        // JOptionPane = pop up a standard dialog box that prompts users for a value
+        //               or informs them of something.
+        
+        //                                                                           icon for the message
+        JOptionPane.showMessageDialog(null, "This is some useless info", "Title", JOptionPane.PLAIN_MESSAGE);
+        
+        int answer = JOptionPane.showConfirmDialog(null, "bro, do you even code?", "MyTitle", JOptionPane.YES_NO_CANCEL_OPTION);
+        
+        String name = JOptionPane.showInputDialog("What is your name?: ");
+        String[] responses = {"No, you are awesome","Thank You","*blush*"};
+        
+        ImageIcon icon = new ImageIcon("src/lobo.png");
+        JOptionPane.showOptionDialog(null, 
+                                    "Awesome", 
+                                    "Secret Message", 
+                                    JOptionPane.YES_NO_CANCEL_OPTION, 
+                                    JOptionPane.PLAIN_MESSAGE, 
+                                    icon, 
+                                    responses, 
+                                    0);
         
     }
     
--- src/guipractice/MyFrame.java
+++ src/guipractice/MyFrame.java
@@ -4,40 +4,9 @@
  */
 package guipractice;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
 /**
  *
  * @author user
  */
-public class MyFrame implements ActionListener{
-    
-    JFrame frame= new JFrame();
-    JButton myButton = new JButton("New Window");
-    
-    MyFrame(){
-        
-        myButton.setBounds(100, 160, 200, 40);
-        myButton.setFocusable(false);
-        myButton.addActionListener(this);
-        
-        frame.add(myButton);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 420);
-        frame.setLayout(null);
-        frame.setVisible(true);
-    }
-    
-    @Override
-    public void actionPerformed(ActionEvent e){
-    
-        if(e.getSource()==myButton){
-            frame.dispose();
-            NewWindow myWindow = new NewWindow();
-        }
-        
-    }
+public class MyFrame {
 }
